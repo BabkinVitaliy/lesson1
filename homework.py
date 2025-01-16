3c087ffeb50405af9b315093c797476761177a4e
BabkinVitaliy <babkin-vi@mail.ru>
2025-01-16 22:26:59 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,16 @@
+print(12 + 9 - 1)
+# 1 st program арифметика
+print(9**0.5*5)
+# 2 st program логика
+print(9.99>9.98 and 1000!=1000.1)
+# 3 st program школьная задача
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+# 4 st program первый после точки
+print('123.456')
+print(123.456 * 10)
+print(int(1234.56/10))
+
+
+
